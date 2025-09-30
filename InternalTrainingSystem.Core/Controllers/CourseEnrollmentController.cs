0d3355481ad956ca0531cedafeb6b3991dde1c5f
NGUYEN TRUNG DUC <nguyentrungduc15052k2@gmail.com>
2025-09-30 22:07:52 +0700

    them CourseEnrollment controller

---
+++ InternalTrainingSystem.Core/Controllers/CourseEnrollmentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternalTrainingSystem.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseEnrollmentController : ControllerBase
+    {
+    }
+}
