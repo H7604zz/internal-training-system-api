fac1831dfe86fc8fcc367915b368f690cff771df
NGUYEN TRUNG DUC <nguyentrungduc15052k2@gmail.com>
2025-10-01 19:20:39 +0700

    sua ten api

--- InternalTrainingSystem.Core/Controllers/NotificationController.cs
+++ InternalTrainingSystem.Core/Controllers/NotificationController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace InternalTrainingSystem.Core.Controllers
 {
-    [Route("api/notifications")]
+    [Route("api/[controller]")]
     [ApiController]
     public class NotificationController : ControllerBase
     {
