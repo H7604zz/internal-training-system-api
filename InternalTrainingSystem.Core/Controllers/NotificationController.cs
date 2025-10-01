ebaae9ea852f353aae283da89f35386e828c1708
NGUYEN TRUNG DUC <nguyentrungduc15052k2@gmail.com>
2025-10-01 14:24:11 +0700

    add notice to staff

---
+++ InternalTrainingSystem.Core/Controllers/NotificationController.cs
@@ -0,0 +1,43 @@
+﻿using InternalTrainingSystem.Core.Services.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternalTrainingSystem.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IEmailSender _mailService;
+        private readonly ICourseService _couseService;
+        public NotificationController(IUserService userServices, IEmailSender mailService, ICourseService couseService)
+        {
+            _userService = userServices;
+            _mailService = mailService;
+            _couseService = couseService;
+        }
+
+        [HttpPost("{courseId}/notify-eligible-users")]
+        public IActionResult NotifyEligibleUsers(int courseId)
+        {
+            var staffWithoutCertificate = _userService.GetUserRoleStaffWithoutCertificate(courseId);
+
+            if (!staffWithoutCertificate.Any())
+                return NotFound("Không có nhân viên nào cần học khóa này.");
+            var course = _couseService.GetCourseByCouseID(courseId);
+            if (course == null)
+                return NotFound("Không tìm thấy khóa học tương ứng.");
+
+            foreach (var user in staffWithoutCertificate)
+            {
+                _mailService.SendEmailAsync(user.Email!,
+                    "Thông báo mở lớp học " + course.CourseName,
+                    $"Xin chào {user.UserName}, nếu bạn chưa có chứng chỉ cho khóa học {courseId}, vui lòng tham gia lớp học sắp tới.");
+            }
+
+            return Ok(new { Message = "Đã gửi mail cho danh sách nhân viên cần học.", Count = staffWithoutCertificate .Count});
+        }
+    }
+}
