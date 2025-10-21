839376fa802adba36d9c85ba9d8ba121e2639810
dungle2107 <ttd21072004@gmail.com>
2025-10-21 18:45:46 +0700

    update database, detele unused property in Lesson, CourseModule , add LessonProgress

---
+++ InternalTrainingSystem.Core/Models/LessonProgress.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace InternalTrainingSystem.Core.Models
+{
+    public class LessonProgress
+    {
+        [Required]
+        public string UserId { get; set; } = null!;
+
+        [Required]
+        public int LessonId { get; set; }
+
+        public bool IsDone { get; set; } = false;
+        public DateTime? StartedAt { get; set; }
+        public DateTime? CompletedAt { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
+
+        [ForeignKey(nameof(LessonId))]
+        public Lesson Lesson { get; set; } = null!;
+    }
+}
