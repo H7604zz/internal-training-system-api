28e8c628f64dc1b4f3047817452e579b472184cd
NGUYEN TRUNG DUC <nguyentrungduc15052k2@gmail.com>
2025-10-30 17:05:48 +0700

    tao lop hoc, tao thoi khoa bieu

--- InternalTrainingSystem.Core/Services/Interface/IClassService.cs
+++ InternalTrainingSystem.Core/Services/Interface/IClassService.cs
@@ -5,7 +5,14 @@ namespace InternalTrainingSystem.Core.Services.Interface
 {
     public interface IClassService
     {
-        Task<PagedResult<ClassDto>> GetClassesAsync(GetAllClassesRequest request);
-        Task<List<ClassDto>> CreateClassesAsync(CreateClassesDto createClassesDto);
-    }
+        Task<(bool Success, List<ClassDto>? Data)> CreateClassesAsync(CreateClassRequestDto request,
+             List<StaffConfirmCourseResponse> confirmedUsers);
+        Task<(bool Success, string Message, int Count)> CreateWeeklySchedulesAsync(CreateWeeklyScheduleRequest request);
+
+        Task<ClassScheduleResponse> GetClassScheduleAsync(int classId);
+        Task<UserScheduleResponse> GetUserScheduleAsync(string staffId);
+        Task<List<ClassEmployeeDto>> GetUserByClassAsync(int classId);
+        Task<ClassDto?> GetClassDetailAsync(int classId);
+        Task<List<ClassDto>> GetClassesByCourseAsync(int courseId);
+     }
 }
