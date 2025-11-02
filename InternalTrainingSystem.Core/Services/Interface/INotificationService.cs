3a159c0fecd2d97ac1dc65129ea270ebb08b9c09
NGUYEN TRUNG DUC <nguyentrungduc15052k2@gmail.com>
2025-11-02 19:21:37 +0700

    update tự động xóa toàn bộ thông báo

--- InternalTrainingSystem.Core/Services/Interface/INotificationService.cs
+++ InternalTrainingSystem.Core/Services/Interface/INotificationService.cs
@@ -7,7 +7,7 @@ namespace InternalTrainingSystem.Core.Services.Interface
         Task SaveNotificationAsync(Notification notification, List<string>? userIds = null, List<string>? roleNames = null);
         Notification? GetNotificationByCourseAndType(int courseId, NotificationType type);
         Notification? GetNotificationByClassAndType(int classId, NotificationType type);
-        public bool HasRecentNotification(NotificationType type, int courseId, int days = 7);
+        Task<bool> HasRecentNotification(NotificationType type, int courseId, int? days = 7);
         Task DeleteOldNotificationsAsync(int courseId, NotificationType type);
         Task<List<Notification>> GetNotificationsAsync(string? userId = null, string? roleName = null);
     }
