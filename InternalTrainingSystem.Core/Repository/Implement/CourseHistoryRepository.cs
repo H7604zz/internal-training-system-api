a2ebbd1eb072e9f86d615e19417361944320263e
HoangAnh-23 <anhnqhhe170508@fpt.edu.vn>
2025-11-05 22:00:21 +0700

    them dto va repository cho lay ra lich su lam bai trong 1 course cua user

---
+++ InternalTrainingSystem.Core/Repository/Implement/CourseHistoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using InternalTrainingSystem.Core.DB;
+using InternalTrainingSystem.Core.DTOs;
+using InternalTrainingSystem.Core.Models;
+using InternalTrainingSystem.Core.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace InternalTrainingSystem.Core.Repository.Implement
+{
+	public class CourseHistoryRepository : ICourseHistoryRepository
+	{
+		private readonly ApplicationDbContext _context;
+
+		public CourseHistoryRepository(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IEnumerable<UserQuizHistoryDto>> GetUserQuizHistoryAsync(string userId, int courseId)
+		{
+			return await _context.CourseHistories
+				.Include(x => x.QuizAttempt)
+				.Where(h => h.UserId == userId && h.CourseId==courseId &&
+				(h.Action == CourseAction.QuizCompleted || h.Action == CourseAction.QuizPassed 
+				|| h.Action == CourseAction.QuizFailed))
+				.Select(h => new UserQuizHistoryDto
+				{
+					QuizId = h.QuizId ?? 0,
+					Action = h.Action,
+					Score = h.QuizAttempt != null ? h.QuizAttempt.Score : 0,
+					StartTime = h.QuizAttempt.StartTime,
+					SubmissionTime = h.QuizAttempt.EndTime
+				})
+				.OrderByDescending(x => x.SubmissionTime)
+				.ToListAsync();
+		}
+	}
+}
---
+++ InternalTrainingSystem.Core/Repository/Interface/ICourseHistoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using InternalTrainingSystem.Core.DTOs;
+
+namespace InternalTrainingSystem.Core.Repository.Interface
+{
+	public interface ICourseHistoryRepository
+	{
+		Task<IEnumerable<UserQuizHistoryDto>> GetUserQuizHistoryAsync(string userId, int courseId);
+	}
+}
