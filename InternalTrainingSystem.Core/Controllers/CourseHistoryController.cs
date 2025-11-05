2ad7156125c106f2b987075a91bfc2aed0bedb16
HoangAnh-23 <anhnqhhe170508@fpt.edu.vn>
2025-11-05 22:01:00 +0700

    them api cho viec lay lich su lam bai trong 1 course cua user

---
+++ InternalTrainingSystem.Core/Controllers/CourseHistoryController.cs
@@ -0,0 +1,27 @@
+﻿using InternalTrainingSystem.Core.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace InternalTrainingSystem.Core.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CourseHistoryController : ControllerBase
+	{
+		private readonly ICourseHistoryService _courseHistoryService;
+
+		public CourseHistoryController(ICourseHistoryService courseHistoryService)
+		{
+			_courseHistoryService = courseHistoryService;
+		}
+		[HttpGet("{courseId}")]
+		public async Task<IActionResult> GetUserCourseHistory(int courseId)
+		{
+			var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			if (string.IsNullOrEmpty(userId))
+				return Unauthorized();
+			var result = await _courseHistoryService.GetUserQuizHistoryAsync(userId, courseId);
+			return Ok(result);
+		}
+	}
+}
