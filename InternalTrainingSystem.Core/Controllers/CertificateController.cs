457808102e1facd520142cf5285da312d87b63ac
H7604zz <hoaphung10112003@gmail.com>
2025-11-15 03:16:18 +0700

    k dùng api để gửi certificate nữa
    gọi thẳng trong CourseService

--- InternalTrainingSystem.Core/Controllers/CertificateController.cs
+++ InternalTrainingSystem.Core/Controllers/CertificateController.cs
@@ -17,47 +17,12 @@ namespace InternalTrainingSystem.Core.Controllers
     {
         private readonly ICertificateService _certificateService;
         private readonly IUserService _userService;
-        private readonly string _baseUrl;
 
         public CertificateController(ICertificateService certificateService,
             IUserService userServices, IConfiguration config)
         {
             _certificateService = certificateService;
             _userService = userServices;
-            _baseUrl = config["ApplicationSettings:ApiBaseUrl"] ?? "http://localhost:7001";
-        }
-
-        [HttpPost("{courseId}/issue-certificate")]
-        public async Task<IActionResult> IssueCertificate(int courseId)
-        {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
-            var user = await _userService.GetUserProfileAsync(userId);
-
-            if (user == null)
-            {
-                return NotFound("User not found");
-            }
-
-            var result = await _certificateService.IssueCertificateAsync(user.Id, courseId);
-
-            string viewCertificatesUrl = $"{_baseUrl}/profile/certificates";
-
-            string emailContent = $@"
-                Xin chào {user.FullName},<br/><br/>
-                Chúc mừng bạn đã <b>hoàn thành khóa học {result.CourseName}</b>! 🎉<br/><br/>
-                Hệ thống đã cấp cho bạn chứng chỉ hoàn thành khóa học.<br/>
-                Bạn có thể xem hoặc tải chứng chỉ trong trang <a href='{viewCertificatesUrl}'>Hồ sơ cá nhân</a>.<br/><br/>
-                Trân trọng,<br/>
-                <b>Phòng Đào Tạo</b>
-            ";
-
-            Hangfire.BackgroundJob.Enqueue(() => EmailHelper.SendEmailAsync(
-                user.Email!,
-                $"Chúc mừng bạn nhận chứng chỉ khóa học {result.CourseName}",
-                emailContent
-            ));
-
-            return Ok(result);
         }
 
         /// <summary>
